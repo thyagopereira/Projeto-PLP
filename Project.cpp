f3e06e347be1d8f1feea5a95836f090d2aaf8e40
cyberia <thyago.pereira.silva@ccc.ufcg.edu.br>
2019-09-04 11:25:06 -0300

    Design e formulação das structs necessárias

---
+++ Project.cpp
@@ -0,0 +1,82 @@
+
+#include<string>
+#include<iostream>
+using namespace std;
+/* 
+    Não recebi confirmações acerca do uso da biblioteca string ,
+    trata-se de uma biblioteca de C++ , porém já vi pessoas afirmarem
+    que a autorização foi permitida em periodos passados.
+
+    Nota : Aos desenvolvendores  <- devido ao uso da bilioteca string 
+    deve-se usar o Iostream de c++ , tal qual entrada e saída sao trabalhadas
+    por meio de cin  , cout .
+
+
+    Para compilar , deve-se usar  g++ [nome do arquivo].cpp -o target 
+    Para rodar em ambiente unix , diferente do windows , ./target  (pós compilação)
+
+    Tais comandos so funcionam perante a compilação inicial do comando  
+    "using namespace std"
+*/ 
+
+struct data
+{
+    int dia;
+    int mes;
+};
+
+
+struct AnuncioEncontrado
+{   
+    string local;
+    struct data data; 
+    string quemEncontrou;
+    string nomePertence;
+    string descricaoObjeto;
+    
+};
+
+
+struct AnuncioPerdido
+{
+    string ondeEsqueceu;
+    struct data quandoPerdeu; 
+    string dono;
+    string nomePertence;
+    string descricaoObjeto;
+
+};
+
+void showDate(struct data date){
+
+    cout << date.dia << "/" << date.mes <<  endl;
+    
+}
+
+
+struct data  criaData(int dia , int mes){
+    struct data date ;
+    // Pode melhorar metodo de verificação ;  
+    if( (dia > 0 ) && (dia < 31 ) && ( mes > 0) && (mes <= 12) ){
+            date.dia  = dia;
+            date.mes  = mes;
+    }   
+        return date ;
+} ;
+
+
+// O uso da Main é voltado a Ideia de um facade / Controller  ; 
+
+int main(){
+        struct AnuncioEncontrado  encontrados[10];
+        struct AnuncioPerdido  perdidos[10];
+        struct data  date = criaData(25,10);
+        //showDate(date);
+
+
+        
+ return 0 ; 
+}
+
+
+
