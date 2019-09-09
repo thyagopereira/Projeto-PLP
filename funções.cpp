40637c47816addaa09d885504f9b495d262e29ec
VictorVirgolino <victor.virgolino@ccc.ufcg.edu.br>
2019-09-09 13:26:55 -0300

    fix print bug

--- funções.cpp
+++ funções.cpp
@@ -150,13 +150,14 @@ void listarPerdidos(struct AnuncioPerdido perdidos[] , int sizeofVector){
 void listarAchadosCategoria(struct AnuncioEncontrado encontrados[], int sizeofVector){
       string categoria;
 
+      /*Recebe a Categoria do Usuario*/
       cout << "  " << endl ;
       cout << "Digite a Categoria:" << endl;
       ws(cin);
       getline(cin, categoria);
 
       cout << "  " << endl ;
-      cout << "Lista de Itens Perdidos da Categoria" << categoria << ": " << endl ;
+      cout << "Lista de Itens Perdidos da Categoria " << categoria << ": " << endl ;
       cout << "  " << endl ;
 
       for(int i = 0 ; i < sizeofVector ; i++){
@@ -173,12 +174,13 @@ void listarPerdidosCategoria(struct AnuncioPerdido perdidos[], int sizeofVector)
       string categoria;
 
       /*Recebe a Categoria do Usuario*/
+      cout << "  " << endl ;
       cout << "Digite a Categoria:" << endl;
       ws(cin);
       getline(cin, categoria);
 
       cout << "  " << endl ;
-      cout << "Lista de Itens Encontrados da Categoria" << categoria << ": " << endl ;
+      cout << "Lista de Itens Encontrados da Categoria " << categoria << ": " << endl ;
       cout << "  " << endl ;
 
       for(int i = 0 ; i < sizeofVector ; i++){
